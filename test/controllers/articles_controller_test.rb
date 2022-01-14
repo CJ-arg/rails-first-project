ef7c78d1b1c531d0f16f319f20db4528d212903d
CARLOS J <janoncarlos@gmail.com>
2022-01-14 17:57:14 -0300

    form created

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -16,5 +16,22 @@ test "should get show" do
       assert_select 'h1', @article.title
       assert_select 'p', @article.body
 end
+test "should get new" do
+      get new_article_url
+      assert_response :success
+      assert_select 'h1', 'New Article'
+      assert_select 'form'
+      assert_select 'form div', 3
+end
+test "should create article" do
+      assert_difference("Article.count") do
+      post articles_url
+      end
+      article = Article.last 
+      assert_redirected_to article_path(article)
+      assert_equal 'Stateless', article.title
+      assert_equal 'Code', article.body
+      assert_equal 'Article was successfully created.', flash[:notice]
+end
 
 end
