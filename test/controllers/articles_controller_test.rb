173437a7d0bb74d13a4f07ece48ef797fcd1a0ca
CARLOS J <janoncarlos@gmail.com>
2022-01-14 12:55:30 -0300

    links in articles and view added

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,10 +1,21 @@
 require "test_helper"
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
+def setup 
+      @article = articles(:one)
+end
    test "should get index" do
      get articles_url
      assert_response :success
      assert_select 'h1', 'Articles'
      assert_select 'li', Article.count
    end
+test "should get show" do
+      # get article_url(@article)
+      get "/articles/#{@article.id}"
+      assert_response :success
+      assert_select 'h1', @article.title
+      assert_select 'p', @article.body
+end
+
 end
